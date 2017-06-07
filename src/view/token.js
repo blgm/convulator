e0e008259ae225b32777ef29fdb135f11d78d0ab
George Blue <oscblgm@gmail.com>
2017-06-07 22:36:13 +0100

    Experiment with TypeStyle

--- src/view/token.js
+++ src/view/token.js
@@ -1,20 +1,36 @@
 import React from 'react'
+import {style} from 'typestyle'
 
 export function Token ({value}) {
   return <div className={tokenClassName(value)}>{tokenValue(value)}</div>
 }
 
+const commonStyle = {
+  'font-size': '20px',
+  'padding': '10px 16px',
+  'margin': '2px',
+  'border-radius': '25px',
+  'text-align': 'center'
+}
+
+export const tokenStyle = {
+  number: style(commonStyle, {background: 'paleturquoise'}),
+  operator: style(commonStyle, {background: 'lightblue'}),
+  equals: style(commonStyle, {background: 'palegreen'}),
+  clear: style(commonStyle, {background: 'pink'})
+}
+
 function tokenClassName (value) {
   if (typeof value === 'number') {
-    return 'number'
+    return tokenStyle.number
   } else if (value === '=') {
-    return 'equals'
+    return tokenStyle.equals
   } else if (value === 'C') {
-    return 'clear'
+    return tokenStyle.clear
   } else if (value === 'AC') {
-    return 'clear'
+    return tokenStyle.clear
   } else {
-    return 'operator'
+    return tokenStyle.operator
   }
 }
 
--- src/view/token.spec.js
+++ src/view/token.spec.js
@@ -1,55 +1,55 @@
 /* eslint-env jest */
 import React from 'react'
 import {shallow} from 'enzyme'
-import {Token} from './token'
+import {Token, tokenStyle} from './token'
 
 describe('Token', () => {
   it('can display a number', () => {
     const wrapper = shallow(<Token value={5} />)
     const div = wrapper.find('div')
     expect(div.text()).toBe('5')
-    expect(div.hasClass('number')).toBeTruthy()
+    expect(div.hasClass(tokenStyle.number)).toBeTruthy()
   })
 
   it('can display an operator', () => {
     const wrapper = shallow(<Token value='+' />)
     const div = wrapper.find('div')
     expect(div.text()).toBe('+')
-    expect(div.hasClass('operator')).toBeTruthy()
+    expect(div.hasClass(tokenStyle.operator)).toBeTruthy()
   })
 
   it('can display an equals sign', () => {
     const wrapper = shallow(<Token value='=' />)
     const div = wrapper.find('div')
     expect(div.text()).toBe('=')
-    expect(div.hasClass('equals')).toBeTruthy()
+    expect(div.hasClass(tokenStyle.equals)).toBeTruthy()
   })
 
   it('displays a division sign', () => {
     const wrapper = shallow(<Token value='/' />)
     const div = wrapper.find('div')
     expect(div.text()).toBe('\u00F7')
-    expect(div.hasClass('operator')).toBeTruthy()
+    expect(div.hasClass(tokenStyle.operator)).toBeTruthy()
   })
 
   it('displays a multiplication sign', () => {
     const wrapper = shallow(<Token value='*' />)
     const div = wrapper.find('div')
     expect(div.text()).toBe('x')
-    expect(div.hasClass('operator')).toBeTruthy()
+    expect(div.hasClass(tokenStyle.operator)).toBeTruthy()
   })
 
   it('can display an Clear sign', () => {
     const wrapper = shallow(<Token value='C' />)
     const div = wrapper.find('div')
     expect(div.text()).toBe('C')
-    expect(div.hasClass('clear')).toBeTruthy()
+    expect(div.hasClass(tokenStyle.clear)).toBeTruthy()
   })
 
   it('can display an Clear All sign', () => {
     const wrapper = shallow(<Token value='AC' />)
     const div = wrapper.find('div')
     expect(div.text()).toBe('AC')
-    expect(div.hasClass('clear')).toBeTruthy()
+    expect(div.hasClass(tokenStyle.clear)).toBeTruthy()
   })
 })
