e83e615908319b7285c2256332b5573cf42b91c6
George Blue <georgeblue@uk.ibm.com>
2017-07-31 22:32:58 +0100

    Move all styling to TypeStyle for consistency

--- src/view/convulator.js
+++ src/view/convulator.js
@@ -1,12 +1,36 @@
 import React from 'react'
 import {Buttons} from './button'
 import {Display} from './display'
+import {cssRule} from 'typestyle'
 
 export function Convulator ({state, dispatcher}) {
+  setupPage()
+
   return (
     <div>
       <Display expression={state.tokens} result={state.result} />
       <Buttons dispatcher={dispatcher} />
     </div>
   )
+}
+
+function setupPage () {
+  // Some style for the staic html page
+  cssRule('body', {
+    background: 'white',
+    fontFamily: 'Arial',
+    display: 'flex',
+    justifyContent: 'center'
+  })
+
+  cssRule('.page', {
+    flexDirection: 'column'
+  })
+
+  cssRule('header', {
+    'fontSize': '30px',
+    'textAlign': 'center',
+    'marginTop': '20px',
+    'marginBottom': '15px'
+  })
 }
